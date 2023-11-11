9dfbb44825a2b89b50dc70925b78de786df1d646
Rz115 <raul7_mh@hotmail.com>
2023-11-11 13:57:50 -0600

    dashboard added

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,12 +5,24 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
-
+/* Trick to repair hybrid refresh view, component over other*/
 const MobileSidebar = () => {
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    },[]);
+
+    if (!isMounted) {
+        return null;
+    }
+
     return(
         <Sheet>
-            <SheetTrigger asChild>
+            <SheetTrigger>
             <Button variant="ghost" size="icon" className="md:hidden">
                 <Menu />
             </Button>
